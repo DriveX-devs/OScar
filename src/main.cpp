141f18260844ba60b071df2ce109493563394381
Francesco Raviglione <francescorav.es483@gmail.com>
2021-11-08 20:13:44 +0100

    First draft of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <string>
+#include <climits>
+#include <cfloat>
 // TCLAP headers
 #include "tclap/CmdLine.h"
 
+#define INVALID_LONLAT -DBL_MAX
+
 int main (int argc, char *argv[]) {
 	std::string dissem_vif = "wlan0";
+	double fixed_lat = -DBL_MAX;
+	double fixed_lon = -DBL_MAX;
 
 	// Parse the command line options with the TCLAP library
 	try {
@@ -20,8 +26,11 @@ int main (int argc, char *argv[]) {
 		// TCLAP::ValueArg<std::string> gntstpropArg("T","gn-tst-prop","Name of the amqp gn-timestamp property",false,"gn_ts","string");
 		// cmd.add(gntstpropArg);
 
-		// TCLAP::ValueArg<int> portArg("P","comm-port","Port for the UDP communication with ms-van3t",false,20000,"int");
-		// cmd.add(portArg);
+		TCLAP::ValueArg<double> fixedLat("L","fixed-lat","Force a fixed latitude value when testing without a GNSS device",false,-DBL_MAX,"float (deg)");
+		cmd.add(fixedLat);
+
+		TCLAP::ValueArg<double> fixedLon("L","fixed-lon","Force a fixed longitude value when testing without a GNSS device",false,-DBL_MAX,"float (deg)");
+		cmd.add(fixedLon);
 
 		// TCLAP::SwitchArg skipGNArg("S","skip-gn","Specify this option to send only Facilities Layer messages, instead of full ITS messages (Facilities layer + GeoNetworking + BTP). Warning! Experimental feature (it will work when only CAMs are sent)!");
 		// cmd.add(skipGNArg);
@@ -29,6 +38,8 @@ int main (int argc, char *argv[]) {
 		cmd.parse(argc,argv);
 
 		dissem_vif=vifName.getValue();
+		fixed_lat=fixedLat.getValue();
+		fixed_lon=fixedLon.getValue();
 
 		std::cout << "[INFO] CAM dissemination interface: " << dissem_vif << std::endl;
 	} catch (TCLAP::ArgException &tclape) { 
