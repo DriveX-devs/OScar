5157c80348f665a4b4a7812817e44d4ec93157f1
Francesco Raviglione <francescorav.es483@gmail.com>
2025-02-17 22:06:10 +0100

    Further improved version of hexToSigned()

--- src/ubx_nmea_parser_single_thread.cpp
+++ src/ubx_nmea_parser_single_thread.cpp
@@ -74,29 +74,24 @@ UBXNMEAParserSingleThread::decimal_deg(double value, char quadrant) {
  *  NOTE: This works only for 3 and 4-dimensioned arrays but it can be extended using
  *  a uint64_t return variable. */
 // TODO: better comment why this function works like this
-int32_t hexToSigned(std::vector<uint8_t> data) {
-    int32_t value = 0;
-
+int32_t
+UBXNMEAParserSingleThread::hexToSigned(std::vector<uint8_t> data) {
     if (data.empty() || data.size() > 4) {
         std::cerr << "Fatal Error: Invalid std::vector data size. Check UBX-ESF-RAW/UBX-NAV-ATT. Terminating. ";
         return -1;
     }
 
+    int32_t value = 0;
+
     for (unsigned int i=0;i<data.size();i++) {
         value = (value << 8) | data[i];
     }
 
-    if(value & (0x01 << (data.size()*8-1))) {
-        uint32_t bitmask=0xff;
+    unsigned int shift = (4 - data.size()) * 8;
 
-        for (unsigned int i=0;i<sizeof(int32_t)-data.size();i++) {
-            bitmask = (bitmask << 8) | 0xff;
-        }
+    value = (value << shift) >> shift;
 
-        value |= bitmask << (data.size()*8);
-    }
-
-    return static_cast<int32_t>(value);
+    return value;
 }
 
 /** clearBuffer() initializes or clears the atomic data buffer
